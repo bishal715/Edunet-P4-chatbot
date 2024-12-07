c4d5aceb57c3dab1511a64752fb185c68977324d
Bishal <sarkarbishal93300@gmail.com>
2024-12-07 15:28:37 +0530

    Added all files

--- app.py
+++ app.py
@@ -9,12 +9,16 @@ import random
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 
+
+
 ssl._create_default_https_context = ssl._create_unverified_context
 nltk.data.path.append(os.path.abspath("nltk_data"))
 nltk.download('punkt')
 
 # Load intents from the JSON file
-file_path = os.path.abspath("./intents.json")
+# file_path = "intents.json"
+file_path = 'P4-Implementation-of-Chatbot-using-NLP/intents.json'
+
 with open(file_path, "r") as file:
     intents = json.load(file)
 
